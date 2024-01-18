c5407726f02b2ae121dc5566a3c7bc74dc29ddfc
Kevin Quail <kevinq.7365@gmail.com>
2024-01-18 13:38:37 -0600

    string functions

--- index.php
+++ index.php
@@ -1,59 +1,30 @@
 <?php
-/*
-| Arithmetic Operators
-| Operator | Description    |
-| -------- | -------------- |
-| `+`      | Addition       |
-| `-`      | Subtraction    |
-| `*`      | Multiplication |
-| `/`      | Division       |
-| `%`      | Modulus        |
-*/
 $output = null;
 
-$num1 = 20;
-$num2 = 10;
+$string = "hello world";
 
-$output = "$num1 + $num2 = " . $num1 + $num2;
-$output = "$num1 - $num2 = " . $num1 - $num2;
-$output = "$num1 * $num2 = " . $num1 * $num2;
-$output = "$num1 / $num2 = " . $num1 / $num2;
-$output = "$num1 % $num2 = " . $num1 % $num2;
-
-//Assignment Operator
-$num3 = 10;
-// $num3 = $num3 + 20;
-$num3 += 20; //These do the same things just 
-
-//Built in PHP Functions
-// rand()
-
-$output = rand();
-$output = getrandmax();
-$output = rand(1, 10);
-
-// round()
-$output = round(4.2);
-
-//ceil
-$output = ceil(4.2); //Always rounds up
-
-//sqrt() square root
-$output = sqrt(64);
-
-//pi() for pi
-
-//abs() for absolute number
-
-//for highest value of numbers in a list max()
-$output = max(1, 2, 3);
-$output = max([1, 2, 3, 25]);
-$output = min([1, 2, 3, 25]);
-
-//Most important as this is good with currency. Its root path 
-
-$output = number_format(1234567.191234, 2, '.', ','); //will equal 1,234,567.19
+//strlen
+$output = strlen($string);
+//str_word_count
+$output = str_word_count($string);
+//strpos
+$output = strpos($string, "world");
+//Get specific char by index
+$output = $string[4];
+//substr
+$output = substr($string, 6 ,5);
+//str_replace
+$output = str_replace("world", "Universe", $string);
+//strtolower
+$output = strtolower($string);
+//strtoupper
+$output = strtoupper($string);
+// ucwords Takes each new word and capitalizes beginning
+$output = ucwords($string);
+//trim Gets rid of spaces
+$output = trim('   World   ')
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -72,8 +43,7 @@ $output = number_format(1234567.191234, 2, '.', ','); //will equal 1,234,567.19
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
-      <?= $output ?>
+      <p class="text-xl"><?= $output ?></p>
     </div>
   </div>
 </body>
