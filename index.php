e5bb484c8bd7b340b065bb0425bfd612e61a1cb4
Kevin Quail <kevinq.7365@gmail.com>
2024-01-22 13:40:32 -0600

    math/array loop challenge. Covers 3 main areas

--- index.php
+++ index.php
@@ -1,91 +1,82 @@
 <?php
-$listings = [
-  [
-    'id' => 1,
-    'title' => 'Software Engineer',
-    'description' => 'We are seeking a skilled software engineer to develop high-quality software solutions.',
-    'salary' => 80000,
-    'location' => 'San Francisco',
-    'tags' => ['Software Development', 'Java', 'Python']
-  ],
-  [
-    'id' => 2,
-    'title' => 'Marketing Specialist',
-    'description' => 'We are looking for a marketing specialist to develop and implement effective marketing strategies.',
-    'salary' => 60000,
-    'location' => 'New York',
-    'tags' => ['Digital Marketing', 'Social Media', 'SEO']
-  ],
-  [
-    'id' => 3,
-    'title' => 'Accountant',
-    'description' => 'We are hiring an experienced accountant to handle financial transactions and ensure compliance.',
-    'salary' => 55000,
-    'location' => 'Chicago',
-    'tags' => ['Accounting', 'Bookkeeping', 'Financial Reporting']
-  ],
-  [
-    'id' => 4,
-    'title' => 'UX Designer',
-    'description' => 'We are seeking a talented UX designer to create intuitive and visually appealing user interfaces.',
-    'salary' => 70000,
-    'location' => 'Seattle',
-    'tags' => ['User Experience', 'Wireframing', 'Prototyping']
-  ],
-  [
-    'id' => 5,
-    'title' => 'Customer Service Representative',
-    'description' => 'We are looking for a friendly customer service representative to assist customers and resolve issues.',
-    'salary' => 40000,
-    'location' => 'New York',
-    'tags' => ['Customer Support', 'Communication', 'Problem Solving']
-  ],
+
+
+/*
+  Challenge 1: Create a multiplication table using a nested `for` loop. The table should look like this:
+
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+1 x 4 = 4
+1 x 5 = 5
+1 x 6 = 6
+1 x 7 = 7
+1 x 8 = 8
+1 x 9 = 9
+1 x 10 = 10
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+... 
+10 x 10 = 100
+*/
+echo '<h3>Multiplication Table</h3>';
+for ($i = 1; $i <= 10; $i++) {
+  for ($j = 1; $j <= 10; $j++){
+    echo $i . ' x ' . $j . ' = ' . $i * $j . '<br>';
+  }
+}
+
+
+/*
+  Challenge 2: Get the sum of the numbers in an array by using a foreach loop. For bonus points, also use a for loop.
+*/
+
+echo '<h3>Array Sum</h3>';
+
+$numbers = [1, 2, 3, 4, 5];
+
+$sum = 0;
+
+foreach($numbers as $number) {
+  $sum += $number;
+}
+echo $sum;
+echo '<br>';
+$sum2 = 0;
+for($i = 0; $i < count($numbers); $i++){
+  $sum2 += $numbers[$i];
+}
+echo $sum2;
+/*
+  Challenge 3: Calculate the average students grade from an array of students. Each student has their own array with the key 'grades'. 
+
+  	1. Create an array of students with their names and grades (0 - 100)
+	2. Iterate over the students array with a foreach loop
+	3. Calculate the average grade for each student
+*/
+
+echo '<h3>Average Grade</h3>';
+
+$students = [ [
+  "name" => "John",
+  "grades" => [85, 90, 92, 88]
+],
+[
+  "name" => "Jane",
+  "grades" => [75, 100, 72, 83]
+],
+[
+  "name" => "Joe",
+  "grades" => [56, 67, 60, 100]
+]
 ];
-?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>Job Listings</title>
-</head>
-
-<body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">Job Listings</h1>
-    </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <!-- Output -->
-    <?php foreach($listings as $listing) : ?>
-    <div class="md my-4">
-      <div class="bg-white rounded-lg shadow-md">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold"><?= $listing["title"] ?></h2>
-          <p class="text-gray-700 text-lg mt-2"><?= $listing["description"] ?></p>
-          <ul class="mt-4">
-            
-            <li class="mb-2">
-              <strong>Salary:</strong> $<?= number_format($listing["salary"], 2) ?>           
-             </li>
-            <li class="mb-2">
-              <strong>Location:</strong> <?= $listing["location"] ?>
-            </li>
-            <li class="mb-2">
-              <strong>Tags:</strong> <?= implode(', ',$listing["tags"]) ?>
-            </li>
-            
-          </ul>
-        </div>
-      </div>
-    </div>
-    <?php endforeach ?>
-  </div>
-</body>
-
-</html>
+foreach($students as $student) {
+  $name = $student['name'];
+  $grades = $student['grades'];
+  //calculate average
+  $average = array_sum($grades) / count($grades);
+  echo "{$name}: Average Grade = {$average} <br>";
+}
