d7cc24d96c9c3f8eafb583f47eaab52981c023a9
Kevin Quail <kevinq.7365@gmail.com>
2024-01-23 09:49:45 -0600

    conditionals in loops, break, and continue functionalities

--- index.php
+++ index.php
@@ -1,39 +1,57 @@
 <?php
 
-$isLoggedIn = false;
-$name = "kevin";
+$number = 1;
+
+
+while($number <= 10) {
+    //if number divided by 2 has zero remained we know its an even number (checking conditional for even number)
+    if ($number % 2 == 0) {
+        echo $number . " is even <br>";
+    } else {
+        echo $number . " is odd <br>";
+    }
+
+    $number++; //!this is to increment or it'd be an infinite loop!!!!
+}
+
+
+//*Break out of a loop
+
+for ($i = 0; $i <=10; $i++) {
+    //So, once the below condition is TRUE then stop the loop(break)
+    if($i == 5) {
+        break;
+    }
+
+    echo $i . "<br>";
+}
+
+//*Skip and continue loop
+
+for ($i = 0; $i <=10; $i++) {
+    //So, once the below condition is TRUE then SKIP the number and continue the loop(continue)
+    if($i == 5) {
+        continue;
+    }
+
+    echo $i . "<br>";
+}
+
+//Conditional in forEach loop in an array
+
+$studentGrades = array(
+    "John" => 75,
+    "Jack" => 92,
+    "Jill" => 100,
+    "Joan" => 80
+);
+
+foreach($studentGrades as $name => $grade) {
+    if($grade >= 90) {
+        echo $name . ", has an excellent grade in the 90s <br>";
+    } else {
+        echo $name . ", you have some work to do <br>";
+    }
+}
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>PHP From Scratch</title>
-</head>
-
-<body class="bg-gray-100">
-  <header class="bg-blue-500 text-white p-4">
-    <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">PHP From Scratch</h1>
-    </div>
-  </header>
-  <div class="container mx-auto p-4 mt-4">
-    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
-      <?php if($isLoggedIn && $name) : ?>
-        <h1 class="text-3xl">Welcome <?= $name ?></h1>
-      <?php elseif($isLoggedIn) : ?>
-        <h1 class="text-3xl">Welcome</h1>
-      <?php else : ?>
-        <h1 class="text-3xl">Please log in</h1>
-
-        <?php endif; ?>
-    </div>
-  </div>
-</body>
-
-</html> 
