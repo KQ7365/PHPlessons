389f2ab9ebd50aa29349826a54179ad04167a4e5
Kevin Quail <kevinq.7365@gmail.com>
2024-01-24 13:31:18 -0600

    constants/global scope

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
 <?php
 
-function add($a = 1, $b = 1) {
-    return $a + $b;
+//two parameters, const name and value
+//defined to make a constant and dont use $
+define("App_Name", "My App");
+define("APP_VERSION", "1.0.0");
+
+//we can also use const!!!!
+
+const DB_NAME = "mydb";
+const DB_HOST = "locahost";
+
+echo DB_HOST, DB_NAME;
+
+function run() {
+    echo App_Name;
+    echo DB_NAME, DB_HOST;
 }
 
-echo add(2, 4);
-echo '<br>';
-echo add(5,6); //Remember, arguments are the values you're passing in. So here the arguments is 5 and 6
-echo '<br>';
-echo add();
-
-//splat operator
-
-function addAll(...$numbers) {
-    $total = 0;
-
-    foreach($numbers as $number) {
-        $total += $number;
-    }
-    return $total;
-}
-echo '<br>';
-echo addAll(1,2,3,4);
+run();
