06be3c3cc95484fc98f4a3b616cab0adb68d48b7
Kevin Quail <kevinq.7365@gmail.com>
2024-01-25 14:51:21 -0600

    public, private, and protected properties within a class

--- index.php
+++ index.php
@@ -1,30 +1,46 @@
 <?php
 
+class User
+{
+  // Properties
+  public $name;
+  public $email;
+  protected $status = "active";
 
-//properties
-class User {
-    //this is creating the keys of an object
-    public $name;
-    public $email;
+  public function __construct($name, $email)
+  {
+    $this->name = $name;
+    $this->email = $email;
+  }
 
-    //constructor -- allows us to shorthand making an object (see below)
-    public function __construct($name, $email){
-       $this->name = $name;
-       $this->email = $email;
-    }
+  // Methods
+  public function login()
+  {
+    echo $this->name . ' logged in <br>';
+  }
 
-    //Methods--this is now a function that runs inside the class
-    public function login(){
-        echo $this->name ." logged in";
-    }
+  //Getter
+  public function getStatus(){
+    echo $this->status;
+  }
+
+  //Setter
+  public function setStatus($status){
+    $this->status = $status;
+  }
 }
 
-//now instantiate/create new user object
-$user1 = new User("John Doe", "john@gmail.com");
+// Instantiate a new object
+$user1 = new User('John Doe', 'john@gmail.com');
 
-
-
-//notice the arrow syntax we use for this--we are now accessing the method inside the Class
 $user1->login();
 
-$user2 = new User("Jane Doe", "jane@gmail.com");
+$user2 = new User('Jane Doe', 'jane@gmail.com');
+
+$user2->login();
+
+$user2->setStatus("inactive");
+
+$user2->getStatus();
+
+$user1->status;
