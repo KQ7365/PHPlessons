62c29f94a00abe8220eff4fa5895f2f98209b592
Kevin Quail <kevinq.7365@gmail.com>
2024-01-29 15:43:43 -0600

    Abstract classes

--- index.php
+++ index.php
@@ -1,23 +1,60 @@
 <?php
 
-class StringUtility {
+abstract class Shape {
 
-    public static function shout($string) {
-        return strtoupper($string . "!");
+    protected $name;
+
+    //Abstract Method
+    abstract public function calculateArea();
+
+    public function __construct($name) {
+        $this->name = $name;
     }
 
-    public static function whisper($string) {
-        return strtolower($string . ".");
+    //Concrete method ((ALSO, we can use this method anywhere below))
+    public function getName(){
+        return $this->name;
+    } 
+
+}
+
+class Circle extends Shape {
+
+        //if you extend class that has abstract method, you have to implement that method
+
+        private $radius;
+
+        public function __construct($name, $radius)
+        {
+            parent::__construct($name);
+            $this->radius = $radius;
+        }
+
+        public function calculateArea()
+        {
+            return pi() * pow($this->radius, 2);
+        }
+}
+
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($name, $height, $width)
+    {
+        parent::__construct($name);
+        $this->height = $height;
+        $this->width = $width;
     }
 
-    public static function repeat($string, $times = 2) {
-        return str_repeat($string, $times);
+    public function calculateArea()
+    {
+        return $this->width * $this->height;
     }
 }
 
-$stringExample = "Let's test this out";
-echo StringUtility::shout($stringExample);
-echo("<br>");
-echo StringUtility::whisper($stringExample);
-echo("<br>");
-echo StringUtility::repeat($stringExample);
+$circle = new Circle("Circle", 5);
+$rectangle = new Rectangle("Reactangle", 4, 6);
+
+echo $circle->getName() . " Area: " . $circle->calculateArea() . "<br>";
+echo $rectangle->getName() . " Area: " . $rectangle->calculateArea() . "<br>";
