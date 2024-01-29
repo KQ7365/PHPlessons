9d2d58c67dffd5754970f826394ef4a398ee2620
Kevin Quail <kevinq.7365@gmail.com>
2024-01-29 15:30:44 -0600

    Challenge/exercise static utility Classes

--- index.php
+++ index.php
@@ -1,30 +1,23 @@
 <?php
 
-class Article {
+class StringUtility {
 
-    private $published = false;
-    public $title;
-    public $content;
+    public static function shout($string) {
+        return strtoupper($string . "!");
+    }
 
-    public function __construct($title, $content) {
+    public static function whisper($string) {
+        return strtolower($string . ".");
+    }
 
-        $this->title = $title;
-        $this->content = $content;
+    public static function repeat($string, $times = 2) {
+        return str_repeat($string, $times);
     }
-            //setter
-    public function publish() {
-        $this->published = true;
-    }
-        //getter
-    public function isPublished() {
-       return $this->published;
-    }
-  
 }
 
-$firstTitle = new Article("Title 1", "Title 1 content");
-$firstTitle->publish();
-$secondTitle = new Article("Title 2", "Title 2 contents");
-
-var_dump($secondTitle);
-var_dump($firstTitle);
+$stringExample = "Let's test this out";
+echo StringUtility::shout($stringExample);
+echo("<br>");
+echo StringUtility::whisper($stringExample);
+echo("<br>");
+echo StringUtility::repeat($stringExample);
