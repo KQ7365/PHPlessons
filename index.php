7d56ebcc7b8f6fa7934f39f4d0a183ce57cc7681
Kevin Quail <kevinq.7365@gmail.com>
2024-02-07 15:53:09 -0600

    Iterating data from external database using a fetch all

--- index.php
+++ index.php
@@ -1,16 +1,54 @@
 <?php
 
-setcookie("username", "jdoe", time() + 3600, "/");
+//if this was in different directories, may need to use folders or /
+require "database.php";
+
+//Prepare a SELECT statement ((This starts a 3 step process to fetch all using PDO))
+//$pdo is accessible from database.php. Also, prepare is a method to make a query
+//step 1
+$stmt = $pdo->prepare("SELECT * FROM posts");
+//Execute statement
+//step 2
+$stmt->execute();
+
+//Fetch results
+//step 3 ((The argument PDO::Fetch_Assoc will return data as object. But, we can do this in Database))
+$posts = $stmt->fetchAll();
+
+//lets see the data
+// echo "<pre>";
+// var_dump($results);
+// echo "<pre>";
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <title>PHP Cookies</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Blog</title>
 </head>
 
-<body>
-  <p>Cookie set. <a href="page.php">Go to page.php</a></p>
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-semibold">My Blog</h1>
+    </div>
+  </header>
+  <div class="container mx-auto p-4 mt-4">
+    <?php foreach($posts as $post) : ?>
+    <div class="md my-4">
+      <div class="rounded-lg shadow-md">
+        <div class="p-4">
+          <h2 class="text-xl font-semibold"><?= $post["title"]?></h2>
+          <p class="text-gray-700 text-lg mt-2"><?= $post["body"]?></p>
+        </div>
+      </div>
+    </div>
+    <?php endforeach  ?>
+  </div>
 </body>
 
 </html>
